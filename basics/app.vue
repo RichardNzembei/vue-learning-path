1414448c0b0f55ec02fa495b096733bdc6f02f57
RichardNzembei <richardsonreuben78@gmail.com>
2024-04-27 22:31:43 +0300

    update

---
+++ basics/app.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive,computed } from "vue"
+const author=reactive({
+    name:'richard reuben',
+    books:[
+        'vue1 -docs',
+        'vue2-docs2',
+        'vue3-docs'
+    ]
+})
+//creating a computed ref
+const publishedbooksmessage=computed(()=>{
+    return author.books.length>0? 'Yes':'No'
+})
+
+</script>
+<template>
+    <h1>published books</h1>
+    <p>{{ publishedbooksmessage }}</p>
+</template>  
